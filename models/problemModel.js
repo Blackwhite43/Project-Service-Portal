6ce81940fc222315e35f6f02d0671d4c5ef4e414
Angga Hidayat <angga@ksatriamitra.com>
2023-03-12 07:00:27 +0700

    update-problem-model

--- models/problemModel.js
+++ models/problemModel.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose');
-// Validator for email
-const validator = require('validator');
-
-// SECURITY
-const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
 
 const problemSchema = new mongoose.Schema({
   title: {
     type: String,
     required: [true, 'Please provide a title'],
     maxlength: [50, 'A tour name must have less or equal then 50 characters'],
+    trim: true,
   },
-  description: { type: String, required: [true, 'Please provide a title'] },
+  description: {
+    type: String,
+    required: [true, 'Please provide a description'],
+    trim: true,
+  },
   status: {
     type: String,
     required: [true, 'Please provide a status'],
     enum: ['open', 'closed', 'process'],
+    message: 'Status is either: open, process or closed',
   },
   situation: {
     type: String,
     required: [true, 'Please provide a situation'],
-    enum: ['normal', 'urgent'],
+    enum: {
+      values: ['normal', 'urgent'],
+      message: 'Situation is either: normal or urgent',
+    },
     default: 'normal',
   },
   completionEstimate: {
